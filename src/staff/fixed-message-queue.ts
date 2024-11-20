8f443a961f34997efc2f87d4e472c64c8fe68f62
alyssoncrvg <alyssonpereira41@gmail.com>
2024-11-20 13:04:25 -0300

    fix: fixed message queue embed value calculation

--- src/staff/fixed-message-queue.ts
+++ src/staff/fixed-message-queue.ts
@@ -1,6 +1,7 @@
 import { TextChannel, Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Interaction, AttachmentBuilder } from 'discord.js';
 import { Guild } from '../db/database';
 import path from 'path';
+import config from '../../config';
 
 export const setupFixedMessageQueue = async (client: Client, interaction: Interaction): Promise<void> => {
 
@@ -8,8 +9,8 @@ export const setupFixedMessageQueue = async (client: Client, interaction: Intera
 
   const { options } = interaction
 
-  const channelOption  = options.get('canal')
-  const valueOption  = options.get('valor')
+  const channelOption = options.get('canal')
+  const valueOption = options.get('valor')
 
   if (!channelOption) {
     await interaction.reply({
@@ -38,16 +39,20 @@ export const setupFixedMessageQueue = async (client: Client, interaction: Intera
     minimumFractionDigits: 2,
   });
 
+  const { rate } = config
+
+  const rateNumber = Number(rate)
+
   const embed = new EmbedBuilder()
-      .setColor('#0099ff')
-      .setTitle('1v1 Clássico | Fila de Competição')
-      .setDescription(`Formato\n1v1 Clássico\n\n`)
-      .addFields([
-        { name: 'Valor', value: currencyFormatter.format(value)},
-        { name: 'Jogadores', value: 'Nenhum jogador na fila \n\n', inline: false },
-      ])
-      .setThumbnail('https://cdn.discordapp.com/attachments/1276274460449575021/1276275081722593359/clashBet.jpg?ex=66c8ef4b&is=66c79dcb&hm=1fdd0951cc8461bb6585478dea0badaace2e018428cbe1fe761edc3c70271cb2&')
-      .setFooter({ text: 'Clash Apostas' });
+    .setColor('#0099ff')
+    .setTitle('1v1 Clássico | Fila de Competição')
+    .setDescription(`Formato\n1v1 Clássico\n\n`)
+    .addFields([
+      { name: 'Valor', value: currencyFormatter.format(value + rateNumber) },
+      { name: 'Jogadores', value: 'Nenhum jogador na fila \n\n', inline: false },
+    ])
+    .setThumbnail('https://cdn.discordapp.com/attachments/1276274460449575021/1276275081722593359/clashBet.jpg?ex=66c8ef4b&is=66c79dcb&hm=1fdd0951cc8461bb6585478dea0badaace2e018428cbe1fe761edc3c70271cb2&')
+    .setFooter({ text: 'Clash Apostas' });
 
   const row = new ActionRowBuilder<ButtonBuilder>()
     .addComponents(
@@ -60,7 +65,7 @@ export const setupFixedMessageQueue = async (client: Client, interaction: Intera
         .setLabel('Sair da Fila')
         .setStyle(ButtonStyle.Danger)
     );
-  
+
 
   await channel.send({ embeds: [embed], components: [row] });
 
