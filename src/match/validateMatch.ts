3d2d09e4ad3cd5a2bdb3a3db884641abd7d93f63
alyssoncrvg <alyssonpereira41@gmail.com>
2024-11-21 13:52:43 -0300

    fix: refactor validMatch function for better readability and battle validation

--- src/match/validateMatch.ts
+++ src/match/validateMatch.ts
@@ -8,10 +8,17 @@ import { saveMacth } from "../db/saveMatch";
 
 const { clashRoyaleApiToken } = config;
 
+const findValidBattle = (battles: any[], clashTagUser2: string): any | null => {
+    return battles.find((battle: any) => {
+        const opponentTag = battle.opponent[0]?.tag;
+        return opponentTag === `#${clashTagUser2}`;
+    });
+};
+
 
 export const validMatch = async (user1: string, user2: string,
-     channel: TextChannel, dateChannel: Date, price: number): Promise<Boolean> => {
-    try{
+    channel: TextChannel, dateChannel: Date, price: number): Promise<Boolean> => {
+    try {
 
         const { rate } = config
 
@@ -25,61 +32,66 @@ export const validMatch = async (user1: string, user2: string,
             headers: { 'Authorization': `Bearer ${clashRoyaleApiToken}` },
         });
 
-        if(responseUser1.data.length > 0){
-        
-            const oponent1 = responseUser1.data[0].opponent[0].tag
+        if (responseUser1.data.length > 0) {
 
-            const tag2 = `#${clashTagUser2}`
+            const battle = findValidBattle(responseUser1.data, clashTagUser2);
 
-            if(oponent1 === tag2){
-                const battleTimeUtc = responseUser1.data[0].battleTime;
+            if (battle) {
 
-                const formattedBattleTime = battleTimeUtc.replace(/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2}).*Z$/, '$1-$2-$3T$4:$5:$6Z');
-                const battleDate = new Date(formattedBattleTime);
+                if (battle.gameMode?.id === 72000007) {
+                    const battleTimeUtc = responseUser1.data[0].battleTime;
 
-                const localDate = battleDate.toLocaleString('pt-BR', {
-                    timeZone: 'America/Sao_Paulo' // Define o fuso como UTC-3 (horário de Brasília)
-                });
-                
-                // Se `dateChannel` é outro objeto Date, converta para o fuso brasileiro também
-                const dateChannelLocal = dateChannel.toLocaleString('pt-BR', {
-                    timeZone: 'America/Sao_Paulo'
-                });
+                    const formattedBattleTime = battleTimeUtc.replace(/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2}).*Z$/, '$1-$2-$3T$4:$5:$6Z');
+                    const battleDate = new Date(formattedBattleTime);
 
-                if(localDate >= dateChannelLocal){
-                    const crownsUser = responseUser1.data[0].team[0].crowns;
-                    const crownsOponnent = responseUser1.data[0].opponent[0].crowns;
+                    const localDate = battleDate.toLocaleString('pt-BR', {
+                        timeZone: 'America/Sao_Paulo' // Define o fuso como UTC-3 (horário de Brasília)
+                    });
 
-                    if(crownsUser > crownsOponnent){
-                        channel.send(`Parabéns pela vitória <@${user1}>`)
-                        channel.send(`Realizando pagamento...`)
-                        await deposito(user1, (price-rateNumber)*2)
-                        channel.send(`Pagamento realizado com sucesso`)
-                        await saveMacth(channel.id, clashTagUser1, clashTagUser2, user1, localDate);
-                    } else if(crownsUser < crownsOponnent){
-                        channel.send(`Parabéns pela vitória <@${user2}>`)
-                        channel.send(`Realizando pagamento...`)
-                        await deposito(user2, (price-rateNumber)*2)
-                        channel.send(`Pagamento realizado com sucesso`)
-                        await saveMacth(channel.id, clashTagUser1, clashTagUser2, user2, localDate);
-                    } else{
-                        channel.send(`Que empate frenético foi esse????? Estaremos reembolsando os seus valores!`)
-                        await deposito(user1, (price-rateNumber))
-                        await deposito(user2, (price-rateNumber))
-                        await saveMacth(channel.id, clashTagUser1, clashTagUser2, 'empate', localDate);
+                    // Se `dateChannel` é outro objeto Date, converta para o fuso brasileiro também
+                    const dateChannelLocal = dateChannel.toLocaleString('pt-BR', {
+                        timeZone: 'America/Sao_Paulo'
+                    });
+
+                    if (localDate >= dateChannelLocal) {
+                        const crownsUser = responseUser1.data[0].team[0].crowns;
+                        const crownsOponnent = responseUser1.data[0].opponent[0].crowns;
+
+                        if (crownsUser > crownsOponnent) {
+                            channel.send(`Parabéns pela vitória <@${user1}>`)
+                            channel.send(`Realizando pagamento...`)
+                            await deposito(user1, (price - rateNumber) * 2)
+                            channel.send(`Pagamento realizado com sucesso`)
+                            await saveMacth(channel.id, clashTagUser1, clashTagUser2, user1, localDate);
+                        } else if (crownsUser < crownsOponnent) {
+                            channel.send(`Parabéns pela vitória <@${user2}>`)
+                            channel.send(`Realizando pagamento...`)
+                            await deposito(user2, (price - rateNumber) * 2)
+                            channel.send(`Pagamento realizado com sucesso`)
+                            await saveMacth(channel.id, clashTagUser1, clashTagUser2, user2, localDate);
+                        } else {
+                            channel.send(`Que empate frenético foi esse????? Estaremos reembolsando os seus valores!`)
+                            await deposito(user1, (price - rateNumber))
+                            await deposito(user2, (price - rateNumber))
+                            await saveMacth(channel.id, clashTagUser1, clashTagUser2, 'empate', localDate);
+                        }
+                        return true;
+
+                    } else {
+                        channel.send(`Insconsistência na data da partida!!`)
                     }
-                    return true;
-
-                } else{
-                    channel.send(`Insconsistência na data da partida!!`)
+                } else {
+                    channel.send(`Partida não é um clássico 1x1`)
                 }
-            } 
-        } else{
+            } else{
+                channel.send(`Partida não encontrada`)
+            }
+        } else {
             channel.send(`erro ao verificar`)
         }
         return false;
 
-    } catch(error){
+    } catch (error) {
         console.log(error)
         return false;
     }
