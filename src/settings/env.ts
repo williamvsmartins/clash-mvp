297f318479fc5af0957cdc983f3529fc3d89cc39
alyssoncrvg <alyssonpereira41@gmail.com>
2025-07-14 09:10:00 -0300

    chore: update env.ts

--- src/settings/env.ts
+++ src/settings/env.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import { z } from "zod";
 
 const envSchema = z.object({
     BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
-    WEBHOOK_LOGS_URL: z.string().url().optional(),
+    // WEBHOOK_LOGS_URL: z.string().url().optional(),
     MONGO_URI: z.string({ description: "MongoDb URI is required" }).min(1),
     // Env vars...
 });
