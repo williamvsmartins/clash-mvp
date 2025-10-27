b92ccb5adc4b1854f7fc26c66cc0dd3ee00116aa
alyssoncrvg <alyssonpereira41@gmail.com>
2025-10-27 15:59:23 -0300

    feat: add settings.json for color and emoji configurations

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,5 @@
 export * from "./discord/reply.js";
+export * from './use_cases/money.js';
+export * from './use_cases/confirmation.js';
+export * from './use_cases/notion.js';
+export * from './use_cases/match.js';
