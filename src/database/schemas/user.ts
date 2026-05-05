b1b661107042db89836cb091251e4d6b33ed2112
williamvsmartins <williamvaltherprogramador@gmail.com>
2026-05-04 23:42:08 -0300

    refactor: correct default value type for moedas in user schema

--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
     clashTag: String,
-    moedas: { type: Number, default: 0.0 }
+    moedas: { type: Number, default: 0 }
 });
 
 export const User = mongoose.model('User', userSchema);
